e7c6974e060bfb5325518cbfe42e1ee05508ce50
anhvh <hoanganh0292@gmail.com>
2024-04-29 18:22:52 +0700

    Add project ListPrimeNumbers

---
+++ src/CSharpFundamental/ListPrimeNumbers/PrimeService.cs
@@ -0,0 +1,38 @@
+﻿namespace ListPrimeNumbers
+{
+    internal static class PrimeService
+    {
+        public static async Task<List<int>> GetPrimesAsync(int start, int end)
+        {
+            List<int> primes = new();
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i))
+                    primes.Add(i);
+            }
+            return await Task.FromResult(primes);
+        }
+
+        public static IEnumerable<int> GetPrimes(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i))
+                    yield return i;
+            }
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ src/CSharpFundamental/ListPrimeNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace ListPrimeNumbers;
+class Program
+{       
+    static async Task Main(string[] args)
+    {
+        int startNumber = 0;
+        int batchSize = 10;
+
+        while (true)
+        {
+            Console.Write("Enter maximum number: ");
+            if (!int.TryParse(Console.ReadLine(), out var endNumber))
+            {
+                Console.WriteLine("Invalid number" + Environment.NewLine);
+                continue;
+            }
+
+            Console.Write("Enter batch size: ");
+            if (!int.TryParse(Console.ReadLine(), out batchSize))
+            {
+                Console.WriteLine("Invalid number" + Environment.NewLine);
+                continue;
+            }
+
+            var sw = new Stopwatch();            
+            
+            //Async
+            sw.Start();
+            List<Task<List<int>>> tasks = new();
+            for (int i = startNumber; i <= endNumber; i += batchSize)
+            {
+                int chunkEnd = Math.Min(i + batchSize - 1, endNumber);
+                tasks.Add(PrimeService.GetPrimesAsync(i, chunkEnd));
+            }
+            
+            var primeLists = await Task.WhenAll(tasks);
+
+            sw.Stop();
+            
+            var asyncPrimes = primeLists.SelectMany(p => p);
+ 
+            Console.WriteLine($"[Async] {asyncPrimes.Count()} Prime numbers between {startNumber} and {endNumber} in {sw.Elapsed.TotalMilliseconds}: {string.Join(", ", asyncPrimes)}");
+
+            //Sync
+            sw.Restart();
+            var syncPrimes = new List<int>();
+            for (int i = startNumber; i <= endNumber; i += batchSize)
+            {
+                int chunkEnd = Math.Min(i + batchSize - 1, endNumber);
+                syncPrimes.AddRange(PrimeService.GetPrimes(i, chunkEnd));
+            }
+            // Output the prime numbers
+            Console.WriteLine($"[Sync] {syncPrimes.Count} Prime numbers between {startNumber} and {endNumber} in {sw.Elapsed.TotalMilliseconds}: {string.Join(", ", syncPrimes)}");
+
+
+        }
+    }
+}
